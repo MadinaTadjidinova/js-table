94cb174c793414ff2a1e7638358894d4db16f1af
Madina Tadjidinova <madina.tadjidinova@gmail.com>
2021-01-26 22:09:10 +0600

    Tryed make table

---
+++ scripts.js
@@ -0,0 +1,15 @@
+for (let i1 = 2; i1 <= 9; i1++) {
+   for (let i2 = 2; i2 <= 9; i2++) {
+    document.write(i1 + " * " + i2 + " = " + i1 * i2 + " ||||| " + "<br>");
+} //исп.очень.часто
+document.write("<br>")
+}
+
+
+// for (let i1 = 1; i1 <= 9; i1++) {  
+//     document.write("<div style='float: left; width: 70px; margin-right: 10px'>"); 
+//     for (let i2 = 1; i2 <= 9; i2++) { 
+//         document.write(i2 + "*" + i1 + "=" +(i2*i1) + "<br>"); 
+//     }  
+//     document.write ("</div>"); 
+// } 
