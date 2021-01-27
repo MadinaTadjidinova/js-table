c4ee7eed728219cea9e088d7b3317d69d547be1a
Madina Tadjidinova <madina.tadjidinova@gmail.com>
2021-01-27 12:40:52 +0600

    Finfished table

--- scripts.js
+++ scripts.js
@@ -1,15 +1,12 @@
-for (let i1 = 2; i1 <= 9; i1++) {
-   for (let i2 = 2; i2 <= 9; i2++) {
-    document.write(i1 + " * " + i2 + " = " + i1 * i2 + " ||||| " + "<br>");
+for (let i1 = 2; i1 <= 10; i1++) {
+   for (let i2 = 2; i2 <= 10; i2++) {
+
+    document.write( "<table><td>" + i1 + " * " + i2 + " = " + i1 * i2 + "</td></table>");
 } //исп.очень.часто
 document.write("<br>")
 }
 
 
-// for (let i1 = 1; i1 <= 9; i1++) {  
-//     document.write("<div style='float: left; width: 70px; margin-right: 10px'>"); 
-//     for (let i2 = 1; i2 <= 9; i2++) { 
-//         document.write(i2 + "*" + i1 + "=" +(i2*i1) + "<br>"); 
-//     }  
-//     document.write ("</div>"); 
-// } 
+
+
+
